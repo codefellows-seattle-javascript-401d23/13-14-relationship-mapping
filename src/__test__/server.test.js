433046f2c165dd20f154f639b633ff72a65d0f95
Josiah Green <JosiahGreen90@gmail.com>
2018-05-02 16:14:23 -0700

    tests are passing

--- src/__test__/server.test.js
+++ src/__test__/server.test.js
@@ -48,6 +48,24 @@ describe('/api/gallerys', () => {
         expect(response.status).toEqual(400);
       });
   });
+
+  test('POST - 409 due to duplicate title', () => {
+    return createGalleryMock()
+      .then((gallery) => {
+        const mockGallery = {
+          galleryName: gallery.galleryName,
+          artists: gallery.artists,
+          residency: gallery.residency,
+        };
+        return superagent.post(apiURL)
+          .send(mockGallery);
+      })
+      .then(Promise.reject)
+      .catch((err) => {
+        expect(err.status).toEqual(409);
+      });
+  });
+
   describe('GET /api/gallerys', () => {
     test('should respond with 200 if there are no errors', () => {
       let galleryToTest = null;
@@ -72,6 +90,61 @@ describe('/api/gallerys', () => {
           expect(response.status).toEqual(404);
         });
     });
+  });
+  describe('PUT /api/gallerys', () => {
+    test('should update a gallery and return a 200 status code', () => {
+      let galleryToUpdate = null;
+      return createGalleryMock()
+        .then((galleryMock) => {
+          galleryToUpdate = galleryMock;
+          return superagent.put(`${apiURL}/${galleryMock._id}`)
+            .send({ galleryName: 'Testing one two three' });
+        })
+        .then((response) => {
+          expect(response.status).toEqual(200);
+          expect(response.body.galleryName).toEqual('Testing one two three');
+          expect(response.body.artists).toEqual(galleryToUpdate.artists);
+          expect(response.body._id).toEqual(galleryToUpdate._id.toString());
+        });
+    });
+
+    test('PUT should send a 400 if there is an invalid request', () => {
+      const galleryToUpdate = {
+        artists: faker.lorem.words(20),
+        residency: faker.lorem.words(20),
+      };
+      return superagent.post(apiURL)
+        .send(galleryToUpdate)
+        .then(Promise.reject)
+        .catch((response) => {
+          expect(response.status).toEqual(400);
+        });
+    });
+
+    test('PUT should respond with 404 if there is no gallery to update', () => {
+      return superagent.get(`${apiURL}/invalidId`)
+        .then(Promise.reject)
+        .catch((response) => {
+          expect(response.status).toEqual(404);
+        });
+    });
+
+    test('PUT - 409 due to duplicate galleryName or artist', () => {
+      return createGalleryMock()
+        .then((gallery) => {
+          const galleryToUpdate = {
+            galleryName: gallery.galleryName,
+            artists: gallery.artists,
+            residency: gallery.residency,
+          };
+          return superagent.post(apiURL)
+            .send(galleryToUpdate);
+        })
+        .then(Promise.reject)
+        .catch((err) => {
+          expect(err.status).toEqual(409);
+        });
+    });
     describe('DELETE /api/gallerys', () => {
       test('should respond with 204 if there are no errors', () => {
         return createGalleryMock() 
@@ -89,23 +162,6 @@ describe('/api/gallerys', () => {
             expect(response.status).toEqual(404);
           });
       });
-    });
-  });
-  describe('PUT /api/gallerys', () => {
-    test('should update a gallery and return a 200 status code', () => {
-      let galleryToUpdate = null;
-      return createGalleryMock()
-        .then((galleryMock) => {
-          galleryToUpdate = galleryMock;
-          return superagent.put(`${apiURL}/${galleryMock._id}`)
-            .send({ galleryName: 'Testing one two three' });
-        })
-        .then((response) => {
-          expect(response.status).toEqual(200);
-          expect(response.body.galleryName).toEqual('Testing one two three');
-          expect(response.body.artists).toEqual(galleryToUpdate.artists);
-          expect(response.body._id).toEqual(galleryToUpdate._id.toString());
-        });
     });
   });
 });
