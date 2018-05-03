4250da248483edb9a4570ae25c77229be65580c1
zachary schumpert <buphnezz@gmail.com>
2018-05-02 18:49:26 -0700

    6 tests passing, 1 fail.  Let the refactoring and variable changing begin!

--- src/lib/server.js
+++ src/lib/server.js
@@ -3,35 +3,33 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import logger from './logger';
-import dinosaurRoutes from '../route/dinosaur-router';
-import loggerMiddleware from './logger-middleware';
-import errorMiddleware from './error-middleware';
+import categoryRoutes from '../route/category-router';
+import errorMiddleWare from './error-middleware';
 
 const app = express();
 let server = null;
-// ---------------------------------------------------------------------------------------
-// Zachary - these routes will be read in-order
-// so it's important that our 404 catch-all is the last one (1) link in the chain
-app.use(loggerMiddleware); // Zachary - using app level middleware
-// ---------------------------------------------------------------------------------------
-// (2) link in the chain
-app.use(dinosaurRoutes);
-// Zachary - making sure I reutrn a 404 status if I don't have a matching route
+
+// #1 in chain
+app.use(categoryRoutes);
+
+// chain 2
 app.all('*', (request, response) => {
-  logger.log(logger.INFO, 'Returning a 404 from the catch-all/default route');
+  logger.log(logger.INFO, 'SERVER: Returning a 404 from the catch-all/default route');
   return response.sendStatus(404);
 });
-// ---------------------------------------------------------------------------------------
-// (3) link in the chain
-app.use(errorMiddleware);
-// ---------------------------------------------------------------------------------------
+
+// chain 3
+app.use(errorMiddleWare);
+
 const startServer = () => {
   return mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
-      // Zachary - once I'm here, I know that mongoose is connected
       server = app.listen(process.env.PORT, () => {
         logger.log(logger.INFO, `Server is listening on port ${process.env.PORT}`);
       });
+    })
+    .catch((err) => {
+      logger.log(logger.ERROR, `something happened, ${JSON.stringify(err)}`);
     });
 };
 
@@ -41,6 +39,9 @@ const stopServer = () => {
       server.close(() => {
         logger.log(logger.INFO, 'Server is off');
       });
+    })
+    .catch((err) => {
+      logger.log(logger.ERROR, `something happened, ${JSON.stringify(err)}`);
     });
 };
 
