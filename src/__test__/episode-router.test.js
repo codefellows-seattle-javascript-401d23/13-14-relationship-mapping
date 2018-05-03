c7dbf06733acdb0f0b5027612a1d10556d82db80
Sarah Geyer <sjgeyer@comcast.net>
2018-05-03 16:22:50 -0700

    all episode router functions written and passing tests

--- src/__test__/episode-router.test.js
+++ src/__test__/episode-router.test.js
@@ -3,7 +3,7 @@
 import faker from 'faker';
 import superagent from 'superagent';
 import { startServer, stopServer } from '../lib/server';
-import { removeMockEpisodeProm } from './lib/episode-mock';
+import { createMockEpisodeProm, removeMockEpisodeProm } from './lib/episode-mock';
 import { createMockPodcastProm } from './lib/podcast-mock';
 
 const apiUrl = `http://localhost:${process.env.PORT}/api/episodes`;
@@ -27,6 +27,94 @@ describe('/api/episodes', () => {
         })
         .then((res) => {
           expect(res.status).toEqual(200);
+          expect(res.body._id).toBeTruthy();
+          expect(res.body.timestamp).toBeTruthy();
+        });
+    });
+    test('400', () => {
+      const episodeToPost = {
+        title: faker.lorem.words(5),
+        description: faker.lorem.words(15),
+      };
+      return superagent.post(apiUrl)
+        .send(episodeToPost)
+        .then(Promise.reject)
+        .catch((res) => {
+          expect(res.status).toEqual(400);
+        });
+    });
+    test('409', () => {
+      return createMockEpisodeProm()
+        .then((mockObject) => {
+          const duplicateEpisode = {
+            title: mockObject.episode.title,
+            description: faker.lorem.words(10),
+            podcast: mockObject.podcast._id,
+          };
+          return superagent.post(apiUrl)
+            .send(duplicateEpisode);
+        })
+        .then(Promise.reject)
+        .catch((res) => {
+          expect(res.status).toEqual(409);
+        });
+    });
+  });
+  describe('PUT', () => {
+    test('200', () => {
+      let episodeToUpdate = null;
+      return createMockEpisodeProm()
+        .then((mockObject) => {
+          episodeToUpdate = mockObject.episode;
+          return superagent.put(`${apiUrl}/${mockObject.episode._id}`)
+            .send({ title: 'New title' });
+        })
+        .then((res) => {
+          expect(res.status).toEqual(200);
+          expect(res.body.title).toEqual('New title');
+          expect(res.body.description).toEqual(episodeToUpdate.description);
+        });
+    });
+  });
+  describe('GET', () => {
+    test('200', () => {
+      let episodeToCompare = null;
+      return createMockEpisodeProm()
+        .then((mockObject) => {
+          episodeToCompare = mockObject.episode;
+          return superagent.get(`${apiUrl}/${mockObject.episode._id}`);
+        })
+        .then((res) => {
+          expect(res.status).toEqual(200);
+          expect(res.body.title).toEqual(episodeToCompare.title);
+          expect(res.body.description).toEqual(episodeToCompare.description);
+          expect(res.body._id).toEqual(episodeToCompare._id.toString());
+        });
+    });
+    test('404', () => {
+      return superagent.get(`${apiUrl}/BADID`)
+        .then(Promise.reject)
+        .catch((res) => {
+          expect(res.status).toEqual(404);
+        });
+    });
+  });
+  describe('DELETE', () => {
+    test('204', () => {
+      return createMockEpisodeProm()
+        .then((mockObject) => {
+          return superagent.del(`${apiUrl}/${mockObject.episode._id}`);
+        })
+        .then((res) => {
+          expect(res.status).toEqual(204);
+          expect(res.body).toEqual({});
+        });
+    });
+    test('404', () => {
+      return superagent.del(`${apiUrl}/BADID`)
+        .then(Promise.reject)
+        .catch((res) => {
+          expect(res.status).toEqual(404);
         });
     });
   });
--- src/route/episode-router.js
+++ src/route/episode-router.js
@@ -10,6 +10,7 @@ const episodeRouter = new Router();
 const jsonParser = bodyParser.json();
 
 episodeRouter.post('/api/episodes', jsonParser, (req, res, next) => {
+  logger.log(logger.INFO, 'EPISODE-ROUTER POST: Processing a request');
   if (!req.body.title) {
     logger.log(logger.INFO, 'EPISODE-ROUTER POST: No title provided');
     return next(new HttpErrors(400, 'Title required'));
@@ -18,6 +19,37 @@ episodeRouter.post('/api/episodes', jsonParser, (req, res, next) => {
     .then((episode) => {
       logger.log(logger.INFO, 'EPISODE-ROUTER POST: 200');
       return res.json(episode);
+    })
+    .catch(next);
+});
+
+episodeRouter.put('/api/episodes/:id', jsonParser, (req, res, next) => {
+  logger.log(logger.INFO, 'EPISODE-ROUTER PUT: Processing a request');
+  const options = { runValidators: true, new: true };
+  return Episode.findByIdAndUpdate(req.params.id, req.body, options)
+    .then((updatedEpisode) => {
+      logger.log(logger.INFO, 'EPISODE-ROUTER PUT: 200');
+      return res.json(updatedEpisode);
+    })
+    .catch(next);
+});
+
+episodeRouter.get('/api/episodes/:id', (req, res, next) => {
+  logger.log(logger.INFO, 'EPISODE-ROUTER GET ONE: Processing a request');
+  return Episode.findById(req.params.id)
+    .then((episode) => {
+      logger.log(logger.INFO, 'EPISODE-ROUTER GET ONE: 200');
+      return res.json(episode);
+    })
+    .catch(next);
+});
+
+episodeRouter.delete('/api/episodes/:id', (req, res, next) => {
+  logger.log(logger.INFO, 'EPISODE-ROUTER DELETE: Processing a request');
+  return Episode.findByIdAndRemove(req.params.id)
+    .then(() => {
+      logger.log(logger.INFO, 'EPISODE-ROUTER: 204');
+      return res.sendStatus(204);
     })
     .catch(next);
 });
